7eb8254d6cc681579f8107206f330f450ce93d7c
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-07 18:21:33 +0200

    Complete Project Setup Part 2 Release #1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
 dotenv.config();
@@ -13,3 +16,24 @@ mongoose
   })
   .then(() => console.log("Database Connection Successful"))
   .catch((error) => console.log(error));
+
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set View Engine
+app.set("view engine", "ejs");
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse Cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// Routing Setup
+
+// Error Handling
+
+app.listen(process.env.PORT, () => {
+  console.log(`App Listening to PORT ${process.env.PORT}`);
+});
