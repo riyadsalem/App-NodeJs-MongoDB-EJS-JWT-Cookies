4d45950fdffdcda414d3330a014461d72231827d
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-28 15:13:01 +0200

    User Management & Validation Part 6 Release #4

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,46 @@
-const People = require("../models/People");
+// External Imports
+const bcrypt = require("bcrypt");
+const path = require("path");
 
-const getUsers = (req, res, next) => {
-  res.render("users");
-};
+// Internal Imposrts
+const User = require("../models/People");
 
-module.exports = { getUsers };
+// Get User From DataBase
+const getUsers = async (req, res, next) => {};
+
+// Add User in to DataBase
+async function addUser(req, res, next) {
+  let newUser;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  if (req.files & (req.files.length > 0)) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+  }
+
+  // Save User or send error
+  try {
+    const result = await newUser.save();
+    res.status(200).json({
+      message: "User was added successfuly",
+    });
+  } catch (err) {
+    res.status(500).json({
+      errors: {
+        common: {
+          meg: "Unknow Error Occured",
+        },
+      },
+    });
+  }
+}
+
+module.exports = { getUsers, addUser };
