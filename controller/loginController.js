71e70630a99f68536624a83de74c9bb82dfe185a
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-12-04 21:15:00 +0200

    JWT - Cooke Authentication PART 1 Release #10

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,7 +1,47 @@
+// External Imports
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+// Internal Imports
+const User = require("../models/People");
+
+// Get Login Page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
+// Do User Login
+async function login(req, res, next) {
+  try {
+    const user = await User.findOne({
+      $or: [{ email: req.body.username }, { mobile: req.body.username }],
+    });
+
+    if (user && user._id) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (isValidPassword) {
+        // prepare the user object to generate token
+        const userObject = {
+          username: user.name,
+          mobile: user.mobile,
+          email: user.email,
+          role: "user",
+        };
+
+        // Generate Token
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 module.exports = {
   getLogin,
+  login,
 };
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 // Internal Imports
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/decorateHtmlResponse");
 
 //Login Page
 router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.post("/login", login);
 
 module.exports = router;
